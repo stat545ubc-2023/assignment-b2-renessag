d4aa2677f095fe8201cfc091beb1831ddbfdbbb3
renessag <renessagomes@hotmail.com>
2023-11-08 22:05:44 -0800

    Added license, function and dependencies. Check passed.

---
+++ R/group_count_and_prop.R
@@ -0,0 +1,22 @@
+#' @title Count and calculate proportions in group(s) 
+#' 
+#' @description Summarizes the number and proportion of observations that meet one or more conditions within given group(s).
+#' 
+#' @param .data A data frame, data frame extension (e.g. a tibble), or a lazy data frame (e.g. from dbplyr or dtplyr).
+#' @param ... Variables to group by.
+#' @param cond Short for "condition". Expressions that return a logical value. Expressions are defined in terms of variables in .data and multiple expressions, if included, must be combined with the & operator. Only rows for which all conditions return TRUE are evaluated.  
+#' 
+#' @return A new data frame summarizing the groups, total number of observations in each group, count of observations that return TRUE for the condition(s) and proportion (%) of observations that return TRUE for the condition(s)
+#' @import dplyr
+#' @export
+#' 
+#' @examples
+#' group_count_and_prop(dplyr::starwars, gender, cond = height > 150 & mass > 50)
+#' group_count_and_prop(mtcars, cyl, cond = mpg > 20)
+
+group_count_and_prop <- function(.data, ..., cond) {
+  .data |> group_by(...) |>
+    summarize(n = n(),
+              count = length(which({{ cond }})),
+              prop = (length(which({{ cond }}))/n())*100)
+}
